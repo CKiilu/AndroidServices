fdfa27240d21c83cc39ed80f9ab69b46b8e8e55b
CKiilu <cndeti@gmail.com>
2016-03-05 14:34:55 +0300

    Added downloadservice with imports

---
+++ app/src/main/java/com/scurrae/chris/androidservices/DownloadService.java
@@ -0,0 +1,24 @@
+package com.scurrae.chris.androidservices;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import android.app.Activity;
+import android.app.IntentService;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
+
+/**
+ * Created by chris on 3/5/16.
+ */
+public class DownloadService extends IntentService {
+}
