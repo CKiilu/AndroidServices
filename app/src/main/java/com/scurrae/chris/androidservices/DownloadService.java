c893ed3844487c4d663ca33a7294c98109e41978
CKiilu <cndeti@gmail.com>
2016-03-06 21:48:01 +0300

    Created new service, HelloService

--- app/src/main/java/com/scurrae/chris/androidservices/DownloadService.java
+++ app/src/main/java/com/scurrae/chris/androidservices/DownloadService.java
@@ -1,24 +1,36 @@
-package com.scurrae.chris.androidservices;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-
-import android.app.Activity;
-import android.app.IntentService;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.os.Message;
-import android.os.Messenger;
-import android.util.Log;
-
-/**
- * Created by chris on 3/5/16.
- */
-public class DownloadService extends IntentService {
-}
+//package com.scurrae.chris.androidservices;
+//
+//import java.io.File;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.net.URL;
+//
+//import android.app.Activity;
+//import android.app.IntentService;
+//import android.content.Intent;
+//import android.net.Uri;
+//import android.os.Bundle;
+//import android.os.Environment;
+//import android.os.Message;
+//import android.os.Messenger;
+//import android.util.Log;
+//
+///**
+// * Created by chris on 3/5/16.
+// */
+//public class DownloadService extends IntentService {
+//    private int result = Activity.RESULT_CANCELED;
+//    public static final String URL = "urlpath";
+//    public static final String FILENAME = "filename";
+//
+//    public DownloadService(String URI){
+//
+//    }
+//
+//    @Override
+//    protected void onHandleIntent(Intent intent) {
+//
+//    }
+//}
---
+++ app/src/main/java/com/scurrae/chris/androidservices/HelloService.java
@@ -0,0 +1,10 @@
+package com.scurrae.chris.androidservices;
+
+import android.app.Service;
+
+/**
+ * Created by chris on 3/6/16.
+ */
+public class HelloService extends Service {
+
+}
